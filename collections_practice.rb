c91b5ad00f4c11a57e03e59fafe2d5819bdd1b56
Azhar Shah <azharshahstl@yahoo.com>
2018-07-19 17:13:12 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(array)
+  array.sort 
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x,y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array.sort {|x,y| x[1] <=> y[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def sum_array(array)
+  array.inject{|sum,n| sum + n}
+end
+
+def add_s(array)
+  array.collect do |word|
+    
+  
+end
